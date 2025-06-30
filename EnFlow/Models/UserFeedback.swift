37e14b30fca7bc0e6e5bbccfe9cf9af834535dd8
orionasc <oriongoodman03@gmail.com>
2025-06-30 10:56:46 -1000

    Refactor feedback card to new energy levels

--- EnFlow/Models/UserFeedback.swift
+++ EnFlow/Models/UserFeedback.swift
@@ -1,10 +1,22 @@
 import Foundation
 
+enum EnergyLevel: Int, Codable, CaseIterable {
+    case low = 1
+    case moderate = 2
+    case high = 3
+
+    var label: String {
+        switch self {
+        case .high: return "High"
+        case .moderate: return "Moderate"
+        case .low: return "Low"
+        }
+    }
+}
+
 struct DailyFeedback: Identifiable, Codable {
     let id: UUID
     let date: Date
-    var feltHighEnergy: Bool
-    var feltStressed: Bool
-    var feltWellRested: Bool
+    var energyLevel: EnergyLevel?
     var note: String?
 }
